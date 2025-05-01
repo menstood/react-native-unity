c5b0593a773959b4a4dae2b05534e470d8e16e9b
menstood <menstood@gmail.com>
2025-05-01 15:58:51 +0700

    feat: add post-build processing for Android to remove intent-filters from AndroidManifest.xml

---
+++ unity/Edtior/PostProcessBuild/PostProcessingBuildAndroid.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using System.IO;
+using System.Xml;
+
+public class PostProcessingBuildAndroid
+{
+    [PostProcessBuild(1)]
+    public static void OnPostBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        if (target != BuildTarget.Android) return;
+
+        var manifestPath = Path.Combine(pathToBuiltProject, "src", "main", "AndroidManifest.xml");
+        if (!File.Exists(manifestPath)) return;
+
+        XmlDocument manifest = new XmlDocument();
+        manifest.Load(manifestPath);
+
+        XmlNode appNode = manifest.SelectSingleNode("/manifest/application");
+
+        if (appNode != null)
+        {
+            foreach (XmlNode activityNode in appNode.SelectNodes(".//activity"))
+            {
+                var intentFilters = activityNode.SelectNodes("intent-filter");
+                for (int i = intentFilters.Count - 1; i >= 0; i--)
+                {
+                    activityNode.RemoveChild(intentFilters[i]);
+                }
+            }
+        }
+
+        manifest.Save(manifestPath);
+        Debug.Log("Removed intent-filters from AndroidManifest.xml Success!.");
+    }
+}
